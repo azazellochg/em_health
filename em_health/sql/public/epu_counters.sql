01389c3250a4341d09eaefda3f3c2b060f3c665f
Grigory Sharov <sharov.grigory@gmail.com>
2025-08-29 15:06:15 +0100

    add skipped image counter

--- em_health/sql/public/epu_counters.sql
+++ em_health/sql/public/epu_counters.sql
@@ -7,22 +7,35 @@ WITH image_counter_param AS (
     SELECT instrument_id, param_id AS image_counter_param_id
     FROM parameters
     WHERE param_name = 'CompletedExposuresCount' AND subsystem = 'EPU'
+),
+
+skip_counter_param AS (
+    SELECT instrument_id, param_id AS skip_counter_param_id
+    FROM parameters
+    WHERE param_name = 'SkippedExposuresCount' AND subsystem = 'EPU'
 )
+
 SELECT
     seg.instrument_id,
     seg.start_time,
     seg.end_time,
     seg.end_state_value,
-    agg.total_image_counter
+    agg.total_image_counter,
+    agg.skip_image_counter
 FROM epu_sessions seg
-         JOIN image_counter_param ic ON ic.instrument_id = seg.instrument_id
-         JOIN LATERAL (
+JOIN image_counter_param ic ON ic.instrument_id = seg.instrument_id
+JOIN skip_counter_param sc ON sc.instrument_id = seg.instrument_id
+JOIN LATERAL (
     SELECT
-        (MAX(d.value_num) - MIN(d.value_num)) AS total_image_counter
+        (MAX(CASE WHEN d.param_id = ic.image_counter_param_id THEN d.value_num END)
+         - MIN(CASE WHEN d.param_id = ic.image_counter_param_id THEN d.value_num END)
+        ) AS total_image_counter,
+        MAX(CASE WHEN d.param_id = sc.skip_counter_param_id THEN d.value_num END) AS skip_image_counter
     FROM data d
     WHERE d.instrument_id = seg.instrument_id
-      AND d.param_id = ic.image_counter_param_id
-      AND d.time >= seg.start_time AND d.time < seg.end_time
-    ) agg ON TRUE
+      AND d.param_id IN (ic.image_counter_param_id, sc.skip_counter_param_id)
+      AND d.time >= seg.start_time
+      AND d.time < seg.end_time
+) agg ON TRUE
 WHERE agg.total_image_counter > 0
 ORDER BY seg.instrument_id, seg.start_time
