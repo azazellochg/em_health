37d3a81eea237d0e84af06c1058f33e088a188ee
Grigory Sharov <sharov.grigory@gmail.com>
2025-10-09 16:23:53 +0100

    1) use log scale for acq speed, 2) add search maps counter for tomo

--- em_health/sql/public/tomo_counters.sql
+++ em_health/sql/public/tomo_counters.sql
@@ -3,8 +3,9 @@
    Image counter for tomo resets to "1" multiple times over the session course.
    We need to sum all peaks before reset and the last peak value.
 
-   The TS counter resets to 0 multiple times over the session course.
+   The TS and Search maps counter resets to 0 multiple times over the session course.
    We need to sum all peaks before reset.
+
       Depends on tomo_sessions view
 */
 CREATE MATERIALIZED VIEW IF NOT EXISTS tomo_counters AS
@@ -18,16 +19,24 @@ WITH image_counter_param AS (
          SELECT instrument_id, param_id AS ts_counter_param_id
          FROM parameters
          WHERE param_name = 'TiltSeriesNumberOfPositions' AND subsystem = 'Tomography'
+     ),
+
+     sm_counter_param AS (
+         SELECT instrument_id, param_id AS sm_counter_param_id
+         FROM parameters
+         WHERE param_name = 'SearchMapsPerJob' AND subsystem = 'Tomography'
      )
 
 SELECT
     seg.instrument_id,
     seg.session_id,
     img_agg.total_image_counter::integer,
-    ts_agg.total_ts_counter::integer
+    ts_agg.total_ts_counter::integer,
+    sm_agg.total_sm_counter::integer
 FROM tomo_sessions seg
          JOIN image_counter_param ic ON ic.instrument_id = seg.instrument_id
          LEFT JOIN ts_counter_param ts ON ts.instrument_id = seg.instrument_id
+         LEFT JOIN sm_counter_param sm ON sm.instrument_id = seg.instrument_id
          JOIN LATERAL (
     WITH seg_data AS (
         SELECT
@@ -78,6 +87,28 @@ FROM tomo_sessions seg
         COALESCE(SUM(peak), 0) AS total_ts_counter
     FROM reset_peaks
     ) ts_agg ON TRUE
+
+         JOIN LATERAL (
+    WITH seg_data AS (
+        SELECT
+            d.time,
+            d.value_num,
+            LAG(d.value_num) OVER (ORDER BY d.time) AS prev_value
+        FROM data d
+        WHERE d.instrument_id = seg.instrument_id
+          AND d.param_id = sm.sm_counter_param_id
+          AND d.time >= seg.start_time
+          AND d.time < seg.end_time
+    ),
+         reset_peaks AS (
+             SELECT prev_value AS peak
+             FROM seg_data
+             WHERE value_num = 0 AND prev_value IS NOT NULL
+         )
+    SELECT
+        COALESCE(SUM(peak), 0) AS total_sm_counter
+    FROM reset_peaks
+    ) sm_agg ON TRUE
 
 WHERE img_agg.total_image_counter > 0
 ORDER BY seg.instrument_id, seg.session_id
