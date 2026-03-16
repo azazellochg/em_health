d6361726d30a523035dad9365ee817b376339266
Grigory Sharov <sharov.grigory@gmail.com>
2026-03-16 16:43:14 +0000

    update schema to remove deprecated mview

--- em_health/sql/migrations/003.sql
+++ em_health/sql/migrations/003.sql
@@ -6,9 +6,12 @@ BEGIN
     SELECT MAX(version) INTO current_version FROM public.schema_info;
 
     IF current_version = 2 THEN
-        -- tbd
+        -- 1. Remove deprecated mview
+        DROP MATERIALIZED VIEW IF EXISTS public.tem_off CASCADE;
+        SELECT delete_job(job_id) FROM timescaledb_information.jobs WHERE proc_name='refresh_tem_off';
+        DROP PROCEDURE IF EXISTS public.refresh_tem_off;
 
-        -- N. Update schema version
+        -- 2. Update schema version
         UPDATE public.schema_info SET version = 3;
     END IF;
 END $$
