1e23bedad1e79e2e2043b4a84191248b464f96e0
Grigory Sharov <sharov.grigory@gmail.com>
2026-03-27 13:09:57 +0000

    fix migration

--- em_health/sql/migrations/004.sql
+++ em_health/sql/migrations/004.sql
@@ -10,7 +10,7 @@ BEGIN
         ALTER TABLE pganalyze.index_stats DROP COLUMN exclusively_locked;
 
         -- 2. Increase chunk size for main table
-        SELECT set_chunk_time_interval('public.data', INTERVAL '7 days');
+        PERFORM set_chunk_time_interval('public.data', INTERVAL '7 days');
 
         -- 3. Update schema version
         UPDATE public.schema_info SET version = 4;
